e5e38a0332f5a4e47d09bfc5599a50ec3b562e11
webjohy <rinowinestudio@gmail.com>
2023-05-27 10:34:51 -0300

    ultimas modificaciones

--- src/app/componentes/boton-editar/boton-editar.component.ts
+++ src/app/componentes/boton-editar/boton-editar.component.ts
@@ -10,3 +10,36 @@ export class BotonEditarComponent {
    
 
 }
+
+// public onOpenModalEditar(modo:String, educacion?: Educacion):void{
+//   const container=document.getElementById('main-container');
+//   const button=document.createElement('button');
+  
+
+//   button.type="button";
+//   button.style.display='none';
+//   button.setAttribute('data-toggle', 'modal');
+//   if(modo==='fotoPortada'){
+//     button.setAttribute('data-target','');
+//     this.modal = new window.bootstrap.Modal(document.getElementById(""));
+//     this.modal.show();
+
+//   } else if (modo==='fotoPerfil'){
+//     this.eliminarEducacion=educacion;
+//     button.setAttribute('data-target','#eliminarEducacionModal');
+//     this.modal = new window.bootstrap.Modal(document.getElementById("eliminarEducacionModal"));
+//     this.modal.show();
+  
+
+//   } else if (modo==='editar'){
+//     this.editarEducacion=educacion;
+//     button.setAttribute('data-target','#editarEducacionModal');
+//     this.modal = new window.bootstrap.Modal(document.getElementById("editarEducacionModal"));
+//     this.modal.show();
+
+//   }
+//   container?.appendChild(button);
+//   console.log(container);
+  
+//   button.click();
+// }
