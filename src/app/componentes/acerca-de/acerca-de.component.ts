aff817569582f0846c0588f1b1d3ea184e2c65e6
Santiago Alessandro <saantialessa@gmail.com>
2023-06-03 11:03:28 -0300

    a la escucha de diferentes propiedades

--- src/app/componentes/acerca-de/acerca-de.component.ts
+++ src/app/componentes/acerca-de/acerca-de.component.ts
@@ -10,17 +10,26 @@ import { PorfolioService } from 'src/app/servicios/porfolio.service';
 export class AcercaDeComponent implements OnInit {
   miPorfolio: any;
   usuarioLogueado: boolean = false;
+  propiedadesModal: any;
+  fotoPerfil: any = { titulo: "Foto de perfil", descripcion: "Nueva URL para foto de perfil" };
+  acercaDe: any = { titulo: "Acerca de", descripcion: "Nueva descripicion sobre mi"};
+  nombreTitulo: any = { titulo: "Nombre Completo", descripcion: "Ingrese su nuevo titulo"};
+
   constructor(private datosPorfolio:PorfolioService) { }
 
-  public editar (){
-    console.log("Editar");
+  public editar (propiedades: string){
+    if(propiedades === 'fotoPerfil'){
+      this.propiedadesModal = this.fotoPerfil;
+    }else if(propiedades === 'acercaDe'){
+      this.propiedadesModal = this.acercaDe;
+    }else if(propiedades === 'nombreTitulo'){
+      this.propiedadesModal = this.nombreTitulo;
+    }
   }
 
   public eliminar (){
     console.log("Eliminar");
   }
-
-  fotoPerfil: any = { titulo: "Foto de perfil", descripcion: "Nueva URL para foto de perfil" };
 
   ngOnInit(): void {
     this.datosPorfolio.obtenerDatos().subscribe(data => {
