7fa315c5739abf2e261f3c331c29995e0f4fb205
webjohy <rinowinestudio@gmail.com>
2023-06-03 10:30:54 -0300

    ultimos cambios botones

--- src/app/componentes/encabezado/encabezado.component.ts
+++ src/app/componentes/encabezado/encabezado.component.ts
@@ -4,23 +4,23 @@ import { Usuario } from 'src/app/models/usuario';
 import { HeaderService } from 'src/app/servicios/header.service';
 import { PorfolioService } from 'src/app/servicios/porfolio.service';
 
+
 @Component({
   selector: 'app-encabezado',
   templateUrl: './encabezado.component.html',
   styleUrls: ['./encabezado.component.css']
 })
 export class EncabezadoComponent implements OnInit {
-
-  public usuario : Usuario | undefined;
+ public usuario : Usuario | undefined;
   public editUsuario : Usuario | undefined;
+  urlImagen: string="";
+  constructor(private headerService : HeaderService, private datosPorfolio:PorfolioService){}
 
-  
-
-  constructor(private headerService : HeaderService){}
-
-  
    ngOnInit(): void {
     this.getUser();
+    this.datosPorfolio.obtenerDatos().subscribe(data => {
+    this.urlImagen = data.imagenBanner; 
+    });
   }
 
   public getUser(): void {
